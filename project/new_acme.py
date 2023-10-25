23c35bf1f80aae3540bd8868f4a10a5c70ddac72
mathislindner <mathis.lindner@gmail.com>
2023-10-25 15:17:56 +0200

    lets rwrite eerythinggg!

---
+++ project/new_acme.py
@@ -0,0 +1,13 @@
+import requests
+from dependencies.jws import jws_creator
+
+class Authentificator():
+    def __init__(self):
+        self.private_key
+        self.public_key
+        
+        
+class Acme():
+    def __init__(self):
+        self.authentificator = authentificator()
+        return None
